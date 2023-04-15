d025e15d08081b7b65b29717a978ccd5a8cbe4cc
juan.soares <juan.soares@darede.com.br>
2023-04-15 12:15:29 -0300

    First commit

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,17 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <ul>
+        <li>
+          <a href="/about/info">Info</a>
+        </li>
+        <li>
+          <a href="/about/contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default About;
